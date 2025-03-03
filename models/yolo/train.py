d30966ed9ebb366a7a53797e444d02dada7d87c1
tudorrw <143077299+tudorrw@users.noreply.github.com>
2025-03-03 22:42:30 +0200

    yolo mode partially done

---
+++ models/yolo/train.py
@@ -0,0 +1,7 @@
+if __name__ == "__main__":
+    from ultralytics import YOLO
+    # Load model
+    model = YOLO("../../configs/yolo/yolo11x.yaml", task="detect").load("../../checkpoints/yolo/yolo11x.pt")  # build from YAML and transfer weights
+    # model = YOLO("configs/yolo/yolov11.yaml", task="detect").load("runs/detect/train6/weights/best.pt")  # build from YAML and load weights
+    # Train model
+    model.train(data="../../configs/yolo/teeth_diagnosis.yaml", epochs=20, imgsz=640, batch=4)  # train
