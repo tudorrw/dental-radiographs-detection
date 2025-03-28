972c45c67709a96b81c7eda31a1f51437de04344
tudorrw <143077299+tudorrw@users.noreply.github.com>
2025-03-28 10:11:48 +0200

    made small changes to yolov11

--- models/yolov11/scripts/train.py
+++ models/yolov11/scripts/train.py
@@ -1,8 +1,8 @@
 import os
 os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
-import albumentations as A
 from ultralytics import YOLO
-
+from ultralytics.data.augment import Albumentations
+from ultralytics.engine.trainer import BaseTrainer
 
 def train_yolo_model():
     BASE_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)))
@@ -10,12 +10,14 @@ def train_yolo_model():
     CFG_PATH = os.path.join(BASE_FOLDER, "..", "teeth_enumeration.yaml")
     SAVE_PATH = os.path.join(BASE_FOLDER, "..", "..", "..", "checkpoints", "yolo")
 
-    model = YOLO(MODEL_PATH, task="detect").load("../../checkpoints/yolo/yolo11x.pt")  # build from YAML and transfer weights
-    # model = YOLO("configs/yolo/yolov11.yaml", task="detect").load("runs/detect/train6/weights/best.pt")  # build from YAML and load weights
-    # Train model
+    model = YOLO(MODEL_PATH, task="detect").load("../../checkpoints/yolo_helpers/yolo11x.pt")  # build from YAML and transfer weights
+    # model.add_callback("on_pretrain_routine_end", callback_custom_albumentations)    # Train model
     os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
-    model.train(data=CFG_PATH, epochs=50, imgsz=640, batch=4, device=0, project=SAVE_PATH)  # train
+    model.train(data=CFG_PATH, epochs=50, imgsz=640, batch=2, device=0, verbose=True, perspective=0.001)  # train
 
 if __name__ == "__main__":
     train_yolo_model()
+
+
+
