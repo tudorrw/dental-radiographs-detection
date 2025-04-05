2949ce50f78737420a10b02b81b9742d3260812b
tudorrw <143077299+tudorrw@users.noreply.github.com>
2025-04-05 12:09:19 +0300

    infos

--- commands.sh
+++ commands.sh
@@ -1,7 +1,21 @@
 # source teeth-yolov11-venv/Scripts/activate
 # cd process
-python -m data.utils.train_test_val_split voc
-python -m data.utils.train_test_val_split coco
+python -m data.utils.train_test_val_split
+python -m data.utils.train_test_val_split
+
 python -m models.faster_rcnn.scripts.train
+python -m models.faster_rcnn.scripts.test
+python -m models.faster_rcnn.scripts.predict
+
+python -m models.yolov11.scripts.train
 python -m models.detr.scripts.train
 python -m models.ssd.scripts.train
+
+python -m models.detr.scripts.visualize_ground_truth
+python -m models.faster_rcnn.scripts.visualize_ground_truth
+#utils
+conda activate dental-dl
+tensorboard --logdir=checkpoints/faster_rcnn
+
+pip install torch torchvision --index-url https://download.pytorch.org/whl/cu124
+tensorboard --logdir=runs/detect/train
